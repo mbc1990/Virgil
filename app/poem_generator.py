1a3f8c15e422380929f9054612d8203fd64b18a2
Malcolm <mbalchcr@oberlin.edu>
2015-02-22 15:31:38 -0500

    static initialization functions for poem generator

--- app/poem_generator.py
+++ app/poem_generator.py
@@ -32,9 +32,6 @@ class Poem_Generator:
         self.seed_words = ['cat', 'winter'] # Input "idea"
         self.poem = poem #ORM object that will be updated as the poem is generated
 
-    @staticmethod
-    def initialize(config):
-        print "Static generator methods initialized!"
 
     # Load parse height cache
     @staticmethod 
@@ -60,6 +57,59 @@ class Poem_Generator:
                 key_map_counter += 1
             output = {'key_map': key_map, 'saved_cache': saved_cache}
             json.dump(output, fp)
+
+    @staticmethod
+    def get_tokens():
+        symbol_stopwords = ['(', ')', '&', '$', '%', '#', '@', '*', '^', '-', '+', '_', '[',']','/', '\\', '=', '|']
+        if os.path.isfile('saved_tokens.json'):
+            print "loading tokens"
+            with open('saved_tokens.json') as toks:
+                tokens = json.load(toks)
+                return tokens
+        else:
+            corpuses = ['god_and_the_state.txt', 'domestic_animals.txt', 'treatise_of_human_nature.txt', 'heart_of_darkness.txt', 'walden.txt','the_prince.txt', 'grimm_fairy_tails.txt', 'kama_sutra.txt', 'tail_of_two_cities.txt', 'ulysses.txt', 'metamorphosis.txt', 'dorian_gray.txt', 'treasure_island.txt', 'the_republic.txt', 'the_time_machine.txt']
+            print "making tokens"
+            tokens = []
+            corpus_counter = 1
+            for c in corpuses:
+                print 'corpus '+str(corpus_counter)+'/'+str(len(corpuses))
+                corpus_counter += 1
+                with open('../corpus/'+c) as fd:
+                    content = fd.read()
+                    sentences = sent_tokenize(content)
+                    for s in sentences:
+                        tokens.append('SENTENCE_START')
+                        tokens += [word for word in word_tokenize(s) if word not in symbol_stopwords]
+            with open('saved_tokens.json', 'w') as outfile:
+                json.dump(tokens, outfile)
+            return tokens
+
+    @staticmethod
+    def build_markov_model(tokens, n):
+        for i in range(0, len(tokens) - n):
+            if 'SENTENCE_START' not in tokens[i:i+n+1]:
+                if tuple(tokens[i:i+n]) not in Poem_Generator.ngram2following_tokens:
+                    Poem_Generator.ngram2following_tokens[tuple(tokens[i:i+n])] = []
+                Poem_Generator.ngram2following_tokens[tuple(tokens[i:i+n])].append(tokens[i+n])
+
+                # ngrams for generating starting "seeds"
+                toks = tokens[i:i+n]
+                for k in toks:
+                    if k not in Poem_Generator.token2ngrams:
+                        Poem_Generator.token2ngrams[k] = []
+                    Poem_Generator.token2ngrams[k].append(toks)
+
+
+    @staticmethod
+    def initialize(config):
+        print "Static generator methods initialized!"
+        Poem_Generator.load_parse_height_cache()
+        tokens = Poem_Generator.get_tokens()
+        print("building model 1")
+        Poem_Generator.build_markov_model(tokens, 1)
+        print("building model 2")
+        Poem_Generator.build_markov_model(tokens, 2)
+
 
     def start_poem(self, poem):
         print "Starting new poem"
